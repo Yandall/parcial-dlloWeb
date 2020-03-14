c4cf8f7ab113e4e02b3bd8664ac548c8b0ffc33b
Yandall <49245907+Yandall@users.noreply.github.com>
2020-03-14 17:39:48 -0500

    Calculo y crud de ICM completo

--- js/controller.js
+++ js/controller.js
@@ -8,10 +8,10 @@ function crearPersona(){
     let personaTemp = obtenerValores()
     if(!existe(personaTemp)){
         console.log("no existe")
-        let ICM = personaTemp.peso / Math.pow(personaTemp.estatura / 100, 2)
-        personaTemp.icm = ICM.toFixed(1)
+        personaTemp.icm = calcularICM(personaTemp.peso, personaTemp.estatura)
         personas.push(personaTemp)
         actualizarTabla()
+        limpiarFormulario()
     } else{
         console.log("existe")
     }
@@ -40,8 +40,8 @@ function obtenerValores(){
     return temporal
 }
 
-function cargarFormulario(idPersona){
-    let personaTemp = personas.find(element => element.id = idPersona)
+function cargarFormulario(indicePersona){
+    let personaTemp = personas[indicePersona]
     document.getElementById("tipoId").value = personaTemp.tipoId
     document.getElementById("id").value = personaTemp.id
     document.getElementById("nombre").value = personaTemp.nombre
@@ -49,6 +49,38 @@ function cargarFormulario(idPersona){
     document.getElementById("correo").value = personaTemp.correo
     document.getElementById("peso").value = personaTemp.peso
     document.getElementById("estatura").value = personaTemp.estatura
+    
+    document.getElementById("btnCrearPersona").style.display = "none"
+    document.getElementById("btnEditarPersona").style.display = "inline"
+    document.getElementById("tipoId").disabled = true
+    document.getElementById("id").disabled = true
+}
+
+function editarPersona(){
+    let personaTemp = obtenerValores()
+    let indice = personas.findIndex(element => element.id == personaTemp.id)
+    personaTemp.icm = calcularICM(personaTemp.peso, personaTemp.estatura)
+    personas.splice(indice, 1, personaTemp)
+    
+    document.getElementById("btnCrearPersona").style.display = "inline"
+    document.getElementById("btnEditarPersona").style.display = "none"
+    document.getElementById("tipoId").disabled = false
+    document.getElementById("id").disabled = false
+    
+    limpiarFormulario()
+    actualizarTabla()
+}
+
+function eliminarPersona(indice) {
+    personas.splice(indice, 1)
+    actualizarTabla()
+
+}
+
+function verEstado(indice) {
+    let personaTemp = personas[indice]
+    if(personaTemp.icm >= 30) alert("Esta persona tiene sobrepeso")
+    else alert("Esta persona no tiene sobrepeso")
 }
 
 function limpiarFormulario(){
@@ -58,26 +90,32 @@ function limpiarFormulario(){
     document.getElementById("apellido").value = ""
     document.getElementById("correo").value = ""
     document.getElementById("peso").value = ""
-    document.getElementById("estatura").value = ""
-    
+    document.getElementById("estatura").value = ""    
 }
+
 
 function actualizarTabla(){
     let lista = document.getElementById("listaPersonas")
     let valores = ""
-    personas.forEach(persona => {
-        valores += `<tr>`
+    for(let i = 0; i < personas.length; i++){
+        let persona = personas[i]
+        valores += `<tr">`
         valores += `<td>${persona.tipoId} ${persona.id}</td>`
         valores += `<td>${persona.nombre} ${persona.apellido}</td>`
         valores += `<td>${persona.correo}</td>`
         valores += `<td>${persona.peso}</td>`
         valores += `<td>${persona.estatura}</td>`
         valores += `<td>${persona.icm}</td>`
-        valores += `<td>    <button type="button" onclick="cargarFormulario(${persona.id})" class="btn btn-warning btn-sm">Editar</button>`
-        valores += `<button type="button" onclick="eliminarPersona(${persona.id})" class="btn btn-danger btn-sm">Eliminar</button> </td>`
+        valores += `<td><button type="button" onclick="cargarFormulario(${i})" class="btn btn-warning btn-sm">Editar</button>`
+        valores += `<button type="button" onclick="eliminarPersona(${i})" class="btn btn-danger btn-sm">Eliminar</button> `
+        valores += `<button type="button" onclick="verEstado(${i})" class="btn btn-info btn-sm">Ver estado</button> </td>`
         valores += `</tr>`
-    })
+    }
     lista.innerHTML = valores
+}
+
+function calcularICM(peso, estatura){
+    return (peso / Math.pow(estatura / 100, 2)).toFixed(1)
 }
 
 
